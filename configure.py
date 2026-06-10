7048a5490be0fa60f79f7dfba9f158484ca083d5
Tilley <tilleyy8@gmail.com>
2026-06-09 20:16:38 -0600

    make more consistent

--- configure.py
+++ configure.py
@@ -1,9 +1,5 @@
 import os, shutil
 
-if not os.getcwd().split("/")[-1] == "tilleys-template-plugin": exit("bad directory")
-
-shutil.rmtree(".git")
-
 plugin_name = input("Plugin name: ")
 description = input("Description: ")
 package_name = input("Package name: ")
@@ -18,32 +14,38 @@ def replace_in_file(path, old, new):
     with open(path, "w", encoding="utf-8") as file:
         file.write(new_content)
 
+base = os.path.join(os.getcwd(), plugin_name.replace(' ', '-'))
 
-plugin_json_path = os.path.join(os.getcwd(), "src", "main", "resources", "rusherhack-plugin.json")
+os.system(f"git clone https://github.com/tillay-rh-plugins/tilleys-template-plugin.git {base}")
+
+shutil.rmtree(os.path.join(base, ".git"))
+os.remove(os.path.join(base, "configure.py"))
+
+plugin_json_path = os.path.join(base, "src", "main", "resources", "rusherhack-plugin.json")
 replace_in_file(plugin_json_path, "URL_HERE", plugin_name.replace(" ", "-"))
 replace_in_file(plugin_json_path, "PACKAGE_HERE", package_name)
 replace_in_file(plugin_json_path, "DESCRIPTION_HERE", description)
 
-gradle_properties_path = os.path.join(os.getcwd(), "gradle.properties")
+gradle_properties_path = os.path.join(base, "gradle.properties")
 replace_in_file(gradle_properties_path, "NAME_HERE", plugin_name.replace(" ", "-"))
 
-readme_path = os.path.join(os.getcwd(), "README.md")
+readme_path = os.path.join(base, "README.md")
 replace_in_file(readme_path, "NAME_HERE", plugin_name.replace("-", " "))
 replace_in_file(readme_path, "DESCRIPTION_HERE", description)
 
 os.rename(
-    os.path.join(os.getcwd(), "src", "main", "java", "lol", "tilley", "PACKAGE_HERE"),
-    os.path.join(os.getcwd(), "src", "main", "java", "lol", "tilley", package_name.replace(" ", "-"))
+    os.path.join(base, "src", "main", "java", "lol", "tilley", "PACKAGE_HERE"),
+    os.path.join(base, "src", "main", "java", "lol", "tilley", package_name.replace(" ", "-"))
 )
 
-main_path = os.path.join(os.getcwd(), "src", "main", "java", "lol", "tilley", package_name.replace(" ", "-"), "PluginMain.java")
+main_path = os.path.join(base, "src", "main", "java", "lol", "tilley", package_name.replace(" ", "-"), "PluginMain.java")
 replace_in_file(main_path, "PACKAGE_HERE", package_name)
 replace_in_file(main_path, "NAME_HERE", plugin_name)
 
-os.rename(os.getcwd(), os.path.join(os.path.dirname(os.getcwd()), plugin_name.replace(" ", "-")))
+os.rename(base, os.path.join(os.path.dirname(base), plugin_name.replace(" ", "-")))
 
 package_directory = os.path.join(
-    os.getcwd(), "src", "main", "java", "lol", "tilley", package_name.replace(" ", "-")
+    base, "src", "main", "java", "lol", "tilley", package_name.replace(" ", "-")
 )
 
 plugin_main_path = os.path.join(package_directory, "PluginMain.java")
@@ -54,7 +56,8 @@ def handle_feature(feature_name, template_class_name, registration_line, suffix)
     if feature_name:
         class_name = f"{feature_name}{suffix}"
         class_path = os.path.join(package_directory, f"{class_name}.java")
-        os.rename(template_path, class_path)
+        new_base = os.path.join(os.path.dirname(base), plugin_name.replace(" ", "-"))
+        os.rename(base, new_base)
         replace_in_file(class_path, template_class_name, class_name)
         replace_in_file(class_path, "PACKAGE_HERE", package_name)
         replace_in_file(plugin_main_path, template_class_name, class_name)
@@ -82,5 +85,3 @@ handle_feature(
     "RusherHackAPI.getCommandManager().registerFeature(new TemplateCommand());\n",
     "Command"
 )
-
-os.system(f"cd ..&&cd {plugin_name.replace(" ", "-")}")
